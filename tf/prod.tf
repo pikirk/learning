760c7f247ce5e75b9fee42d0af4245c86ccbe780
pikirk <paul.kirk@dteenergy.com>
2020-12-31 19:21:57 -0800

    adding nginx web server

--- tf/prod.tf
+++ tf/prod.tf
@@ -37,3 +37,16 @@ resource "aws_security_group" "prod_web"{
         "Terraform" : "true"
     }
 }
+
+resource "aws_instance" "prod_web" {
+    ami = "ami-0527edca851734058"
+    instance_type = "t2.nano"
+
+    vpc_security_group_ids = [
+        aws_security_group.prod_web.id
+    ]
+
+    tags = {
+        "Terraform" : "true"
+    }
+}
