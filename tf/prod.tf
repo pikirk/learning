c372e2ba89d36dfce48656c64e069ccdacab8bcc
pikirk <paul.kirk@dteenergy.com>
2020-12-31 17:40:08 -0800

    course checkpoint - vpc

--- tf/prod.tf
+++ tf/prod.tf
@@ -3,7 +3,9 @@ provider "aws" {
     region  = "us-west-2"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_course" {
     bucket = "tf-course-202012311235"
     acl    = "private"
 }
+
+resource "aws_default_vpc" "default" {}
