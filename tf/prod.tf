5180364efd60f97142d00866734c4d79b2532781
pikirk <paul.kirk@dteenergy.com>
2021-01-04 10:45:41 -0800

    refactor to asg

--- tf/prod.tf
+++ tf/prod.tf
@@ -54,25 +54,6 @@ resource "aws_security_group" "prod_web"{
     }
 }
 
-resource "aws_instance" "prod_web" {
-    count         = 2
-    ami           = "ami-0527edca851734058"
-    instance_type = "t2.nano"
-
-    vpc_security_group_ids = [
-        aws_security_group.prod_web.id
-    ]
-
-    tags = {
-        "Terraform" : "true"
-    }
-}
-
-resource "aws_eip_association" "prod_web" {
-    instance_id    = aws_instance.prod_web[0].id
-    allocation_id  = aws_eip.prod_web.id 
-}
-
 resource "aws_eip" "prod_web" {
     tags = {
         "Terraform" : "true"
@@ -81,7 +62,6 @@ resource "aws_eip" "prod_web" {
 
 resource "aws_elb" "prod_web" {
     name            = "prod-web"
-    instances       = aws_instance.prod_web.*.id
     subnets         = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
     security_groups = [aws_security_group.prod_web.id]
 
@@ -95,4 +75,38 @@ resource "aws_elb" "prod_web" {
     tags = { 
         "Terraform" : "true"
     }
+}
+
+resource "aws_launch_template" "prod_web" {
+    name_prefix   = "prod-web"
+    image_id      = "ami-0527edca851734058"
+    instance_type = "t2.nano"
+    tags = { 
+        "Terraform" : "true"
+    }
+}
+
+resource "aws_autoscaling_group" "prod_web" {
+    # references the subnets in the vpc
+    vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+    desired_capacity   = 1
+    max_size           = 1
+    min_size           = 1
+
+    launch_template {
+        id      = aws_launch_template.prod_web.id
+        version = "$Latest"
+    }
+
+    tag { 
+        key                  = "Terraform" 
+        value                = "true"
+        propagate_at_launch  = true
+    } 
+}
+
+# Create a new load balancer attachment and link to elb
+resource "aws_autoscaling_attachment" "prod_web" {
+    autoscaling_group_name = aws_autoscaling_group.prod_web.id
+    elb                    = aws_elb.prod_web.id
 }
