6ce89cbea4eabce19985dab6b26086d049707a0c
pikirk <paul.kirk@dteenergy.com>
2021-01-04 09:28:17 -0800

    scale up web nodes

--- tf/prod.tf
+++ tf/prod.tf
@@ -39,6 +39,7 @@ resource "aws_security_group" "prod_web"{
 }
 
 resource "aws_instance" "prod_web" {
+    count         = 2
     ami           = "ami-0527edca851734058"
     instance_type = "t2.nano"
 
@@ -51,9 +52,12 @@ resource "aws_instance" "prod_web" {
     }
 }
 
-resource "aws_eip" "prod_web" {
-    instance = aws_instance.prod_web.id
+resource "aws_eip_association" "prod_web" {
+    instance_id    = aws_instance.prod_web[0].id
+    allocation_id  = aws_eip.prod_web.id 
+}
 
+resource "aws_eip" "prod_web" {
     tags = {
         "Terraform" : "true"
     }
